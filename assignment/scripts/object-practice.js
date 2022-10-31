75165c01fc43fa37e43a5db4614f5bebd0655852
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-10-30 19:23:53 -0500

    added question to html file, and completed tasks in js

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -13,7 +13,7 @@ const me = {
   firstName: 'Roxanne',
   lastName: 'Niederkorn',
   hasSiblings: true,
-  shoeCount: 8,
+  shoeCount: 8,   // originally I added + 1 shoes here instead of in the console.log
   favThreeFoods: ['chocolate', 'tamales', 'apple chips']
 };
 console.log('A little about me:', me);
@@ -26,28 +26,34 @@ console.log('A little about me:', me);
 */
 let fullName = (me.firstName + (' ') + me.lastName)
 
-console.log(fullName);
+console.log('My name is: ' ,fullName);
 
 
 
 /* 3. Nested arrays
-  - Console.log your first favorite food
-  - Console.log your last favorite food 
+  X - Console.log your first favorite food
+  X - Console.log your last favorite food 
 */
-
+console.log('One of my favorite foods is: ' ,me.favThreeFoods[[0]]);
+console.log('My final favorite food is: ' ,me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 
 /* 4. Change a property of an existing object.
-  - Console.log your current shoe count.
+  X - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log('I have' , me.shoeCount, 'pairs of shoes.');
 
+//let newShoeCount = (me.shoeCount + 1);    this didn't work the way I hoped after doing the next exercise(*5)
+console.log('I now have', me.shoeCount + 1, 'pairs of shoes.')
 
 /* 5. Add a new property to an existing object.
-  - Add a 'favoriteColor' to your existing object
+  X - Add a 'favoriteColor' to your existing object
   - Do this the same way you updated the variable above
   - Cool huh? It works, even though it wasn't there before
-  - Console.log your object
+ X - Console.log your object
 */
+me.favoriteColor = 'Purple';
+console.log(me.favoriteColor);
